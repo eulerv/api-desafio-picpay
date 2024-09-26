db4511762fb9eddc8439be83a121334956bf77ed
Euler Vicente <euler.vicente@hotmail.com>
2024-09-26 17:49:07 -0300

    Feature:Production branch adjust, correction of a database and orm conflict.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
+-- POSTGRESQL
 INSERT INTO wallets (ID, FULL_NAME, CPF, EMAIL, "password", "type", BALANCE) VALUES (1, 'João - User', 12345678900, 'joao@test.com', '123456', 1, 1000.00);
 INSERT INTO wallets (ID, FULL_NAME, CPF, EMAIL, "password", "type", BALANCE) VALUES (2, 'Maria - Lojista', 12345678901, 'maria@test.com', '123456', 2, 1000.00);
