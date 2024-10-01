7b5e2114c104980b02d2531844b1c1c001f29b80
Euler Vicente <euler.vicente@hotmail.com>
2024-10-01 08:20:58 -0300

    Bug fixing:CORS issue.

--- src/main/java/com/github/eulerv/picpaydesafiobackend/authenticationPackage/cors/CorsConfig.java
+++ src/main/java/com/github/eulerv/picpaydesafiobackend/authenticationPackage/cors/CorsConfig.java
@@ -1,25 +1,16 @@
 package com.github.eulerv.picpaydesafiobackend.authenticationPackage.cors;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+    
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*","/**")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH")
-                        .allowedHeaders("*")
-                        .allowCredentials(true)
-                        .exposedHeaders("Authorization");
+                registry.addMapping("/**");
             }
-        };
-    }
+        
 }
