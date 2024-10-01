d23ee0cbfa678c7eabc55528cf7dc97ace4e61f0
Euler Vicente <euler.vicente@hotmail.com>
2024-10-01 01:05:12 -0300

    Bug fixing:CORS issue.

--- src/main/java/com/github/eulerv/picpaydesafiobackend/authenticationPackage/cors/CorsConfig.java
+++ src/main/java/com/github/eulerv/picpaydesafiobackend/authenticationPackage/cors/CorsConfig.java
@@ -1,25 +1,20 @@
 package com.github.eulerv.picpaydesafiobackend.authenticationPackage.cors;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
+//@Configuration
 public class CorsConfig {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*","/**")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH","*","/**")
-                        .allowedHeaders("*","/**")
-                        .allowCredentials(true)
-                        .exposedHeaders("Authorization","/**");
-            }
-        };
-    }
+    // @Bean
+    // public WebMvcConfigurer corsConfigurer() {
+    //     return new WebMvcConfigurer() {
+    //         @Override
+    //         public void addCorsMappings(CorsRegistry registry) {
+    //             registry.addMapping("/**")
+    //                     .allowedOrigins("*","/**")
+    //                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH","*","/**")
+    //                     .allowedHeaders("*","/**")
+    //                     .allowCredentials(true)
+    //                     .exposedHeaders("Authorization","/**");
+    //         }
+    //     };
+    // }
 }
