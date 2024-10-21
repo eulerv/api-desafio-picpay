933a2d04a1c4a47d96b979c742fd5ae745bb2284
Euler Vicente <euler.vicente@hotmail.com>
2024-10-21 09:10:42 -0300

    Health path adjust.

--- src/main/java/com/github/eulerv/picpaydesafiobackend/scheduler/DatabaseResetScheduler.java
+++ src/main/java/com/github/eulerv/picpaydesafiobackend/scheduler/DatabaseResetScheduler.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 import com.github.eulerv.picpaydesafiobackend.authenticationPackage.model.User;
 import com.github.eulerv.picpaydesafiobackend.authenticationPackage.repository.UserRepository;
@@ -24,9 +23,6 @@ public class DatabaseResetScheduler {
     
     @Autowired
     private PasswordEncoder passwordEncoder;
-    
-    private static final String URL = "https://api-desafio-picpay.onrender.com/wallets";
-    private final RestTemplate restTemplate = new RestTemplate();
 
     // Agendamento RESET diário às 02:00
     @Scheduled(cron = "0 0 2 * * ?", zone = "America/Sao_Paulo")
@@ -38,15 +34,5 @@ public class DatabaseResetScheduler {
         User defaultUser = new User("user", passwordEncoder.encode("123456"));
         userRepository.save(defaultUser);
         System.out.println("Banco de dados resetado com sucesso às 02:00.");
-    }
-
-    @Scheduled(fixedDelay = 25 * 60 * 1000) // To wake up application hosted on render
-    public void sendPingToWakeUp() {
-        try {
-            restTemplate.getForObject(URL, String.class);
-            System.out.println("Ping enviado com sucesso para " + URL);
-        } catch (Exception e) {
-            System.err.println("Erro ao enviar ping: " + e.getMessage());
-        }
     }
 }
